7fe68698d47dcd769033f6b18f09747a8247ce73
Adanta Griffin <griffinadanta90@gmail.com>
2024-06-05 02:14:04 -0400

    dogYears complete

---
+++ app.js
@@ -0,0 +1,9 @@
+let myAge = 33; // Variable representing my age
+let earlyYears = 2; //earlyYears set to 2 PRQ
+earlyYears = earlyYears * 10.5;
+let laterYears = myAge - 2; // my Age minus the first 2 already accounted for in code above earlyears
+laterYears = laterYears * 4; //later years of my age time 4 to calculate laterYears age in dog year.
+let myAgeInDogYears = earlyYears + laterYears;
+let myName = 'ADANTA GRIFFIN';
+myName = myName.toLowerCase(); // returns text lower case
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years`);
